7684a2335cce2cb91eb0c8616e817392b34d903a
Lizliz666 <Lizliz666233@gmail.com>
2023-07-26 08:50:07 +0800

    完整版上传

---
+++ src/main/java/com/qf/qfseckill/config/RedisKey.java
@@ -0,0 +1,9 @@
+package com.qf.qfseckill.config;
+
+public class RedisKey {
+
+    public static final String VERFIY_CODE="verfiy_code:";
+    public static final String VERFIY_NUM="verfiy_num:";
+    public static final String LOGIN_NUM ="login_num:" ;
+
+}
